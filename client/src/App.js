7c96313ed7d77abd32f7b5c5089100d3d12fdacd
RozzaysRed <Ryan.Rowse@gmail.com>
2019-10-25 07:46:12 -0400

    Add client plain subfolder

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Tweets from './components/Tweets'
+import './App.css';
+
+function App() {
+  return (
+    <Tweets />
+  );
+}
+
+export default App;
---
+++ client/src/components/Tweets.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import Tweet from './Tweet'
+import Query from './Query'
+import { QUERY_GET_TWEETS } from '../graphql/queries'
+import './Tweets.css'
+import CreateTweet from './CreateTweet'
+import TwitterLogo from '../assets/twitter.svg'
+
+class Tweets extends React.Component {
+  render() {
+    return (
+      <div className="tweets">
+        {/* <div className="tweet">
+          <div className="author">
+            <img
+              src={'https://api.adorable.io/avatars/190/abott@adorable.png'}
+              alt="user-avatar"
+            />
+            <strong>@RozzaysRed</strong>
+          </div>
+          <div className="content">
+            <div className="twitter-logo">
+              <img src={TwitterLogo} alt="twitter-logo" />
+            </div>
+            <textarea autoFocus className="editTextarea" value="" onChange="" />
+          </div>
+        </div> */}
+        <CreateTweet />
+        <Query query={QUERY_GET_TWEETS} render={Tweet} />
+      </div>
+    )
+  }
+}
+
+export default Tweets
